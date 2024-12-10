32aad76414eadcc359a72419c58712c043f14a72
Dutra0707 <00001107728125sp@al.educacao.sp.gov.br>
2024-12-10 15:44:03 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,22 @@
+criaCartao(
+    'corrida de formula 1',
+    'como funciona a corrida?',
+    'As corridas são sempre realizadas aos domingos, mas a disputa de um GP não se resume apenas à prova. Cada etapa dura um fim de semana inteiro'
+)
+
+criaCartao(
+    'corrida da formula 1',
+    'como são feitos os carros?',
+    'Eles são feitos de fibra de carbono moldada, uma fibra sintética que tem maior resistência ao impacto do que o aço'
+)
+
+criaCartao(
+    'corrida de formula 1',
+    'como é feita a pista da corrida?',
+    'A mais notável de todas as mudanças que foram feitas no tradicional circuito, para trazê-lo aos padrões atuais da categoria, foi virar o trecho final em 34% - o equivalente a 18 graus.'
+)
+    criaCartao(
+    'corrida de formula 1',
+    'Como entrar na formula 1?',
+    'Para chegar à Fórmula 1, é obrigatório passar pelas melhores escolas de pilotagem de F1, credenciadas pela Federação Internacional de Automobilismo (FIA).'
+)
